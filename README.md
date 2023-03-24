fbf2b86ade18ac82268036cd849dc59bdc476e64
Vitalii Babiienko <v.babiyenko@gmail.com>
2023-03-24 16:27:20 +0200

    template version 0.1.0 completed

---
+++ README.md
@@ -0,0 +1,22 @@
+# Vite template React + TS
+
+# Need to change
+
+- inside vite.config.ts: base: '/<repo>/';
+- inside package.json: "homepage": "https://<user>.github.io/<repo>/";
+
+# dependencies
+
+- react;
+- react-redux;
+- @reduxjs/toolkit;
+- sass;
+- classnames;
+
+# devDependencies
+
+- typescript;
+- @types/react;
+- eslint;
+- @mate-academy/eslint-config-react-typescript;
+- gh-pages;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import { App } from './App';
+import { store } from './app/store';
+import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>,
+);
